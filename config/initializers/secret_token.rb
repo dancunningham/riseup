9dc49459cbcbcbc8c828c1941ee0f311a0c86dcd
John Schult <john.schult@worthpoint.com>
2013-11-12 17:17:02 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Riseup::Application.config.secret_key_base = '2339f77546611c8107b8d5be04cb3fa3910bb28390983cf7df40970f6c96409259d6a053f7d2170aebe23179d778d0bd7319d679559a1c3d0ed992b40f492c92'
